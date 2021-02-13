8f81637564f581b789625f63e7de9fd58bee8ffe
Arun Dharavath <53053571+ArunDharavath@users.noreply.github.com>
2021-02-13 22:46:41 +0530

    Tickets Submission

---
+++ ticketsub.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int f=0;
+		string s;
+		cin >> s;
+		if(s[0]==s[1]){ 
+			cout << "NO" << endl; 
+			continue;
+		}
+		else{
+			for(int k=2;k<s.length();k++){
+				if(k%2==0){
+					if(s[k]!=s[0]){
+						f=1;
+						break;
+					}
+				}
+				else{
+					if(s[k]!=s[1]){
+						f=1;
+						break;	
+					}
+				}
+			}
+		}
+	cout<<(f==0?"YES":"NO")<<endl;
+	}
+	return 0;
+}
