319c431d827db39dbe4f3bb30812fdeb4e23fe8d
Arun Dharavath <53053571+ArunDharavath@users.noreply.github.com>
2021-03-01 18:52:53 +0530

    Chef and Friend

---
+++ chef&friends.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int tc, res = 0;
+	cin >> tc;
+	while(tc--)
+	{
+	    string s, s1;
+	    int count = 0;
+	    cin >> s;
+	    s1 = "chef";
+	    for(int i = 0; i < s.length()-1; ++i)
+	    {
+	        for(int j = 0; j < s1.length()-1; ++j)
+	        {
+	            if(s[i] == s1[j] && s[i+1] == s1[j+1])
+	            {
+	                ++count;
+	                break;
+	            }
+	            else
+	                count = 0;
+	        }
+	        if(count > 0)
+	        {
+	            break;
+	        }
+	    }
+	    if(count > 0)
+	        ++res;
+	}
+    cout << res;
+    return 0;
+}
